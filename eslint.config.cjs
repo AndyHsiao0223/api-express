6e7fdded79d07f6516c6b5137ba6c5f09b5204e6
AndyHsiao0223 <Andy90.2.23@gmail.com>
2025-10-15 11:18:20 +0800

    initial commit

---
+++ eslint.config.cjs
@@ -0,0 +1,16 @@
+const js = require('@eslint/js');
+const prettier = require('eslint-config-prettier');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es6,
+      },
+    },
+  },
+];
