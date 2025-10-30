1365b42b03469e066d6db4ff078fb6c596707db2
AndyHsiao0223 <Andy90.2.23@gmail.com>
2025-10-31 00:07:25 +0800

    10/31 stash

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 import express from 'express';
+//  A logging library
+import logger from '#configs/logger.js';
+// A security middleware
+import helmet from 'helmet';
+// An HTTP request logger middleware
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hello world');
+
   res.status(200).send('Hello, World!');
 });
 
